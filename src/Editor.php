904eb1c50ebb18508ac84e9cf779035a722372ea
Daniel Bachhuber <daniel@handbuilt.co>
2017-07-25 16:24:35 -0700

    Add function doc block

--- src/Editor.php
+++ src/Editor.php
@@ -14,6 +14,11 @@ use WP_Error;
  */
 class Editor {
 
+	/**
+	 * Render the 'Convert Text to Speech' media button
+	 *
+	 * @param string $editor_id TinyMCE editor ID.
+	 */
 	public static function action_media_buttons( $editor_id ) {
 		?>
 		<button type="button" class="button simpletts-convert-text" data-editor="<?php echo esc_attr( $editor_id ); ?>">
